2ca81efa3c24a12f9f1e169e3f0d02a196ceb075
benson40111 <benson40111@gmail.com>
2018-11-23 18:41:35 +0800

    Change travis

--- test_server.py
+++ test_server.py
@@ -3,6 +3,7 @@ import requests
 def test_calc():
     req = requests.get('http://localhost:8080/get?speech=123加456')
     text = req.json()
+    print(text)
     assert text['text'] == '123 + 456 = 579'
 
 
