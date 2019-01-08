ab2dc58e340d60163e753cfce95fb0e69a281b68
benson40111 <benson40111@gmail.com>
2019-01-08 13:56:04 +0800

    Change 123 + 456

--- test_server.py
+++ test_server.py
@@ -4,7 +4,7 @@ def test_calc():
     req = requests.get('https://vawsr.mino.tw/nlp/get?speech=123加456')
     text = req.json()
     print(text)
-    assert text['text'] == '123 + 123 = 246'
+    assert text['text'] == '123 + 456 = 579'
 
     req = requests.get('https://vawsr.mino.tw/nlp/get?speech=123加上456等於多少')
-    assert text['text'] == '123 + 123 = 246'
+    assert text['text'] == '123 + 456 = 579'
