02753f73ea4b6c03f9b5170788f14c0c01aba423
benson40111 <benson40111@gmail.com>
2019-01-08 13:17:13 +0800

    Add assert

--- test_server.py
+++ test_server.py
@@ -5,3 +5,6 @@ def test_calc():
     text = req.json()
     print(text)
     assert text['text'] == '123 + 123 = 246'
+
+    req = requests.get('http://localhost:8080/get?speech=123加上456等於多少')
+    assert text['text'] == '123 + 123 = 246'
