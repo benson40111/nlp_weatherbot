9e38d0d46dc63ccb2998d454702d23cb89c8dfeb
benson40111 <benson40111@gmail.com>
2019-01-08 13:25:17 +0800

    Change domainname

--- test_server.py
+++ test_server.py
@@ -1,10 +1,10 @@
 import requests
 
 def test_calc():
-    req = requests.get('http://localhost:8080/get?speech=123加456')
+    req = requests.get('https://vawsr.mino.tw/nlp/get?speech=123加456')
     text = req.json()
     print(text)
     assert text['text'] == '123 + 123 = 246'
 
-    req = requests.get('http://localhost:8080/get?speech=123加上456等於多少')
+    req = requests.get('https://vawsr.mino.tw/nlp/get?speech=123加上456等於多少')
     assert text['text'] == '123 + 123 = 246'
