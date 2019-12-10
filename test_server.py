423395696ca7aed431b0c5a5d2b6d34379366ff5
dirtydirty <benson40111@gmail.com>
2019-12-10 20:07:52 +0800

    Add ignore

--- test_server.py
+++ test_server.py
@@ -1,10 +1,10 @@
 import requests
 
 def test_calc():
-    req = requests.get('https://vawsr.mino.tw/nlp/get?speech=123加456')
+    req = requests.get('localhost:8080/get?speech=123加456')
     text = req.json()
     print(text)
     assert text['text'] == '123 + 456 = 579'
 
-    req = requests.get('https://vawsr.mino.tw/nlp/get?speech=123加上456等於多少')
+    req = requests.get('localhost:8080/get?speech=123加上456等於多少')
     assert text['text'] == '123 + 456 = 579'
